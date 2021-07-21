5c79879ab8f32e276d80168711571efca8454028
nero-15 <nero15.dev@gmail.com>
2021-07-21 21:41:55 +0900

    add contents

--- my-gatsby-site/src/pages/blog.js
+++ my-gatsby-site/src/pages/blog.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({ data }) => {
   return (
@@ -10,6 +11,9 @@ const BlogPage = ({ data }) => {
           <article key={node.id}>
             <h2>{node.frontmatter.title}</h2>
             <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
           </article>
         ))
       }
